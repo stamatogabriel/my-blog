d36ac81df44d6273c659ae26b900fdfe3bfc241b
Gabriel <stamato7@gmail.com>
2021-06-17 09:05:37 -0300

    feat: add information label permitions

--- src/components/SEO/SEO.jsx
+++ src/components/SEO/SEO.jsx
@@ -15,6 +15,13 @@ export default ({ title, description, url, article }) => {
       <Helmet title={seo.title} >
         <meta name="description" content={seo.description} />
         <meta name="image" content={seo.image} />
+
+        <script>{(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-TQGNQ38')}</script>
+
         {seo.url && <meta property="og:url" content={seo.url} />}
         {article ? (
           <meta property="og:type" content="article" />
